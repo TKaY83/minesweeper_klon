d0e3bfc88a2b32303609c827d44aee8780c5d6cf
Tuncay Dagdelen <sramos083@gmail.com>
2022-09-03 13:19:06 +0200

    pictures added

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 let fields = []; //8x8 10b css:94px
-const width = 10;
-const height = 10;
+const width = 8;
+const height = 8;
 const nBombs_easy = 10;
-// const nBombs_easy = 10;
 
-function setBombs() {
+function selectField(x, y) {
+    getFiled(x, y).revealed = true;
+    render();
+};
 
+function getFiled(x, y) {
+    return fields.find(obj => obj.x == x && obj.y == y)
 }
 
-function rendern() {
-    playField = document.getElementById('play_field');
-    playField.innerHTML = ``;
-    playField.innerHTML = `
+function setBombs() {
+    let x, y;
+    let counter = 0;
+    while (counter < nBombs_easy) {
+        x = Math.floor(Math.random() * width);
+        y = Math.floor(Math.random() * height);
+        let field = getFiled(x, y);
+        if (!field.hasBomb) {
+            field.hasBomb = true;
+            counter++;
+        }
+    }
+}
 
-    `;
+function render() {
+    // playField = document.getElementById('play_field');
+    let strId = ``;
+    fields.forEach(obj => {
+        if (obj.revealed) {
+            strId = `box_${obj.x}${obj.y}`;
+            document.getElementById(strId).classList.add('revealed');
+        }
+    });
 }
 
 function initializeFieldData() {
     for (let w = 0; w < width; w++)
         for (let h = 0; h < height; h++)
-            fields.push({ x: w, y: h, number: 0, hasBomb: false });
+            fields.push({ x: w, y: h, number: 0, hasBomb: false, revealed: false });
 }
 
 function buildBoxes() {
@@ -30,7 +51,7 @@ function buildBoxes() {
     for (let w = 0; w < width; w++) {
         zeile = `<div>`;
         for (let h = 0; h < height; h++) {
-            str = `<div class="box" onclick="selectField(${h}, ${w})"></div>`;
+            str = `<div id="box_${w}${h}" class="box" onclick="selectField(${w}, ${h})"></div>`;
             zeile += str;
         };
         zeile += `</div>`;
@@ -46,4 +67,6 @@ function init() {
 
     initializeFieldData();
     buildBoxes();
+    setBombs();
+    render();
 }
